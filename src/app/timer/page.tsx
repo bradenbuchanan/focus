098fdf5b8870fe234bd777c3723a91b3ead0e871
Braden Buchanan <bradenjbuchanan@gmail.com>
2025-03-23 11:42:04 -0400

    update timer page.tsx

--- src/app/timer/page.tsx
+++ src/app/timer/page.tsx
@@ -1,7 +1,6 @@
 // src/app/timer/page.tsx
 import TimerContainer from '@/app/components/timer/TimerContainer';
 import styles from '@/app/components/timer/timer.module.css';
-
 export default function TimerPage() {
   return (
     <div className={styles.timerPage}>
