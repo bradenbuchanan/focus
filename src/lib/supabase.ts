d84af12d22381dac774c3fd1338c09ab37ef9fb7
Braden Buchanan <bradenjbuchanan@gmail.com>
2025-03-30 19:43:20 -0400

    comment out supabaseurl and anonkey for now.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/types/supabase';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
 console.log("Supabase key available:", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
