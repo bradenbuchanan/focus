c7a09fddae7da564158ac03c7a85ca696d05a543
Braden Buchanan <bradenjbuchanan@gmail.com>
2025-05-04 23:04:23 -0400

    commenting out console.logs

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,8 +4,8 @@ import { Database } from '@/types/supabase';
 // const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 // const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("Supabase key available:", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+// console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+// console.log("Supabase key available:", !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL as string,
