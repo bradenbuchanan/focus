7197567a0df4bbf8c6f0c009012ecb91cdd987b5
Braden Buchanan <bradenjbuchanan@gmail.com>
2025-05-12 21:21:49 -0400

    feat: add event system for session completion and data updates

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,8 +1,17 @@
 // src/utils/events.ts
 export const DATA_UPDATED_EVENT = 'focusDataUpdated';
+export const SESSION_COMPLETED_EVENT = 'focusSessionCompleted';
 
 export function emitDataUpdate() {
   window.dispatchEvent(new CustomEvent(DATA_UPDATED_EVENT));
+}
+
+export function emitSessionCompleted(sessionData: { 
+  type: string; 
+  activity: string; 
+  duration: number;
+}) {
+  window.dispatchEvent(new CustomEvent(SESSION_COMPLETED_EVENT, { detail: sessionData }));
 }
 
 export function listenForDataUpdates(callback: () => void) {
@@ -10,5 +19,13 @@ export function listenForDataUpdates(callback: () => void) {
   
   return () => {
     window.removeEventListener(DATA_UPDATED_EVENT, callback);
+  };
+}
+
+export function listenForSessionCompleted(callback: (event: CustomEvent) => void) {
+  window.addEventListener(SESSION_COMPLETED_EVENT, callback as EventListener);
+  
+  return () => {
+    window.removeEventListener(SESSION_COMPLETED_EVENT, callback as EventListener);
   };
 }
